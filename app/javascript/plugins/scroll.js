c12fa1dd60dd81404e6ac8e407082fb1c67c3c50
Eliot Nemoz <nemoz.eliot@gmail.com>
2020-04-17 18:45:18 +0200

    js sc

--- app/javascript/plugins/scroll.js
+++ app/javascript/plugins/scroll.js
@@ -9,6 +9,21 @@ scroll.classList.add('animated', 'fadeInRight')
 },{ once: true });
 // };
 
+window.addEventListener('scroll', (event) => {
+    if (document.documentElement.scrollTop > 400) {
+            console.log('test')
+          }
+});
+
+// const scroll2 = document.querySelector('.bio');
+// console.log(scroll2);
+// addEventListener('scroll', (event) => {
+// console.log('coucou')
+// if scroll2.
+// scroll2.style.display = "block"
+// scroll2.classList.add('animated', 'fadeInRight')
+// },{ once: true });
+
 
 
 export {scroll};
