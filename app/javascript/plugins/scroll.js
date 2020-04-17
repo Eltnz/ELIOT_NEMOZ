751f6c7c42dd54adcc16227280141189736a611c
Eliot Nemoz <nemoz.eliot@gmail.com>
2020-04-17 11:42:14 +0200

    js

--- app/javascript/plugins/scroll.js
+++ app/javascript/plugins/scroll.js
@@ -4,6 +4,8 @@ const scroll =  document.querySelector('.welcome');
 console.log(scroll);
 addEventListener('scroll', (event) => {
 console.log('coucou')
+scroll.style.display = "block"
+scroll.classList.add('animated', 'fadeInRight')
 },{ once: true });
 // };
 
